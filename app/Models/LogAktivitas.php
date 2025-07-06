4d616ddc5646d532eecaa9769db89026a66d4df4
Octora20 <yohanoctora20@gmail.com>
2025-07-06 23:28:39 +0700

    update fitur log activity

---
+++ app/Models/LogAktivitas.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogAktivitas extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'user_name',
+        'aksi',
+        'keterangan'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
